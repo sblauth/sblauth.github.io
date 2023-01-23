b8bd93b309a7c327620a0007669dc956ba66f6df
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-01-23 08:30:27 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -19,10 +19,6 @@ author = 'Sebastian Blauth'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinxarg.ext",
-]
-
 
 highlight_language = "python"
 
